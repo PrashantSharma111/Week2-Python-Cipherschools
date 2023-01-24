db4a04edaf50a01e6a30cf234651ed6665d9a6ea
Prashant12207032 <119326785+PrashantSharma111@users.noreply.github.com>
2023-01-24 20:29:06 +0530

    Add files via upload

---
+++ class11-lambdaIntro-CipherSchools.py
@@ -0,0 +1,11 @@
+show = print
+show("something")
+
+name = ["Prashant","Kumar","Sharma","Dev","Aditya"]
+for name in enumerate(name):
+    print(name)
+
+a = 5
+b = 9
+a,b = b,a
+print(a,b)
---
+++ class12-stringOperations-CipherSchools.py
@@ -0,0 +1,6 @@
+a = 5
+print("value of a is %d" % (a))
+
+name = "Prashant Kumar Sharma"
+company = "Google"
+print("name = {name}\ncompany = {company}".format(name = name , company = company))
